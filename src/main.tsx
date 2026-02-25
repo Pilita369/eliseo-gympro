90219bb590a514b64a0b9ca055fc8c90750ac5b1
Pilar Alvarado <pilar.alvaradob@gmail.com>
2026-02-25 12:04:41 -0300

    mas cambios

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Yo monto la app en el div #root.
 createRoot(document.getElementById("root")!).render(<App />);
